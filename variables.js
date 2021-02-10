e293bfcce228c7929a3b6fb590a3dc104e4e0453
Hoyos Lozano <Alveiro.Hoyos@cencosud.com.co>
2021-02-10 16:29:22 -0500

    2. Hoisting

--- variables.js
+++ variables.js
@@ -3,3 +3,31 @@ var edad
 
 //inicializar
 edad=30
+
+
+//hoisting
+console.log(nombre)
+var nombre = 'alveiro'
+console.log(nombre)
+
+hey()
+
+function hey() {
+    console.log(`hola ${name}`)
+}
+
+var name = 'Alveiro'
+
+// scope global
+
+var name = 'Alveiro'
+
+function nombre () {
+    // scope local
+    var apellido = 'Hoyos'
+    
+    console.log(`${name} ${apellido}`)
+}
+
+nombre()
+console.log(`${apellido}`)
