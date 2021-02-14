817eb3e278c5613518498d15799aad7117c99aa4
Mono <alveiro7@gmail.com>
2021-02-14 12:01:57 -0500

    3. Arrays

---
+++ arrays.js
@@ -0,0 +1,90 @@
+const frutas = ["manzana", "pera", "piña", "tomate"]
+
+frutas.length= 6
+console.log(`tengo ${frutas.length} frutas`)
+console.log(`${frutas}`)
+console.log(`${frutas[1]}`)
+
+
+const ary = new Array("hi", true, 3, [2, false], {name: "Cosme"});
+console.log(ary);
+console.log(ary.length)
+console.log(typeof(ary))
+
+const ary = ["hi", true, 3, [2, false], {name: "Cosme"} ]
+console.log(ary);
+console.log(typeof(ary))
+
+// agregar desde la ultima posicion
+// Push
+const addfrutas = (fruta) =>{
+    addfruta = frutas.push(fruta)
+    console.log(`fruta agregada ${fruta}`)
+}
+
+addfrutas("caramelo")
+console.log(`tengo ${frutas.length} frutas`)
+console.log(`${frutas}`)
+
+// eliminando el ultimo elemento
+// pop
+const deleteFrutas =  () => {
+    console.log(`fruta a eliminar ${frutas[frutas.length-1]}`)
+    deleteFruta = frutas.pop()
+}
+
+deleteFrutas()
+console.log(`ahora tengo ${frutas.length} frutas`)
+console.log(`${frutas}`)
+
+// agregando al inicio
+// unshift
+const addfrutas1 = (fruta) =>{
+    addfruta = frutas.unshift(fruta)
+    console.log(`fruta agregada ${fruta}`)
+}
+
+addfrutas1("patacon")
+console.log(`tengo ${frutas.length} frutas`)
+console.log(`${frutas}`)
+
+
+// eliminando al incio
+// shift
+const deleteFrutas1 =  () => {
+    console.log(`fruta a eliminar ${frutas[0]}`)
+    deleteFruta = frutas.shift()
+}
+
+deleteFrutas1()
+console.log(`ahora tengo ${frutas.length} frutas`)
+console.log(`${frutas}`)
+
+
+// buscar position
+// indexOf
+const position = (fruit) => {
+    location = frutas.indexOf(fruit)
+    console.log(`La fruta ${fruit} se encuentra en la posicion ${location} del array`)
+}
+
+position ("piña")
+
+
+// toString
+const frutas = [12,"piña", 20, 45, "tomate"]
+
+console.log(frutas.toString())
+
+// reverse
+console.log(frutas.reverse())
+
+// sort
+console.log(frutas.sort())
+
+// slice(inicio, fin)
+console.log(frutas.slice(2, 3))
+
+//slice(inicio)
+console.log(frutas.slice(2))
+
--- condicionales.js
+++ condicionales.js
@@ -1,5 +1,77 @@
-// if 
-if (true) {
+// if
+if (false) {
     console.log(`Habla, todo bien`)
-     
+} else {
+    console.log(`Error`)
+}
+
+
+// else if
+edad = "10" ;
+if (edad === 18) {
+    console.log(`puedes votar por la colombia humana porque tienes ${edad} años`)
+} else if (edad === "18") {
+    console.log(`Epa el numero de acabas de colocar es un ${typeof(edad)}`)
+} else if (edad > 18) {
+    console.log(`puede votar por el que diga el patron uribe porque ud tiene ${edad} años`)
+} else {
+    console.log(`aun no puede votar, vaya a ver cuentos de los hermanos green porque tiene ${edad} añitos ${typeof(edad)}`)
+}
+
+
+// operador condicional ternario
+
+edad === 18 ? console.log(`puedes votar por la colombia humana porque tiene ${edad} años`) : console.log(`no puedes votar por nadien`)
+
+
+// pieda papel o tijera
+
+let op1 = "piedra"
+
+let op2 = "papel"
+
+let op3 = "tijera"
+
+const result = (user, cpu) => {
+    if(user != cpu) {
+        if(user === op1 && cpu === op3){
+            console.log(`el usuario GANO con ${op1}`)
+        } else if(user === op2 && cpu === op1) {
+            console.log(`el usario GANO con ${op2}`)
+        } else if(user === op3 && cpu === op2) {
+            console.log(`el usaurio GANO con ${op3}`)
+        } else {
+            console.log(`La CPU Gano!!!`)
+        }
+    } else if (user === cpu) {
+        console.log(`DEUCES`)
+    }
+}
+
+result(op3, op3)
+
+
+// switch
+// piedra papel tijera
+
+const question = prompt("Ingresar tu opcion: piedra, papel o tijera")
+const user = question.toLowerCase()
+const options = ["piedra", "papel", "tijera"]
+const machine = options[Math.floor(Math.random() * 3)]
+
+switch(true) {
+    case (user === machine):
+        console.log(`${user} DEUCES!! ${machine}`)
+        break
+    case (machine === 'piedra' && user === 'papel'):
+        console.log(`${user}  WIN!! ${machine}`)
+        break
+    case (machine === 'papel' && user === 'tijera'):
+        console.log(`${user} WIN!!  ${machine}`)
+        break
+    case (machine === 'tijera'  &&  user === 'piedra'):
+        console.log(`${user} WIN!!! ${machine}`)
+        break
+    default:
+        console.log(`${user} GAME OVER!! ${machine}`)
 }
