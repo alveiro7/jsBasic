bd77a73b39bb28f9fa98cad9b2d25ab15d572dee
Hoyos Lozano <Alveiro.Hoyos@cencosud.com.co>
2025-09-03 10:49:48 -0500

    basico week 1

---
+++ 14-assignments.js
@@ -0,0 +1,10 @@
+/* Values and Variables
+Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+Log their values to the console. */
+
+let country = "Colombia 🇨🇴"
+let continent = "South America"
+let population = "53,494,426"
+
+console.log(`The country of ${country} is located in the continent of ${continent} and its population is ${population}`)
